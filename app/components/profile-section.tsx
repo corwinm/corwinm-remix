f618cfa4f90bf2e41b4a3ea39570ab7a427dae10
Corwin Marsh <corwinm@users.noreply.github.com>
2022-07-04 12:22:26 -0700

    Update profile section animation to whileInView

--- app/components/profile-section.tsx
+++ app/components/profile-section.tsx
@@ -5,8 +5,9 @@ export default function ProfileSection({ children }: { children: ReactNode }) {
   return (
     <motion.section
       initial={{ opacity: 0, translateY: 64 }}
-      animate={{ opacity: 1, translateY: 0 }}
       transition={{ duration: 1.6, delay: 0.6 }}
+      whileInView={{ opacity: 1, translateY: 0 }}
+      viewport={{ once: true }}
     >
       {children}
     </motion.section>
