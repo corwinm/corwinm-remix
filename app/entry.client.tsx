e379d4145840b93547ea8ab5bbbc7002acc8034c
Corwin Marsh <corwinm@users.noreply.github.com>
2022-11-02 20:11:21 -0700

    Enable vercel/analytics now that it should be working

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,17 +1,16 @@
 import { RemixBrowser } from "@remix-run/react";
 import { hydrate } from "react-dom";
-// import { inject } from "@vercel/analytics";
+import { inject } from "@vercel/analytics";
 import { webVitals } from "./web-vitals";
 
 hydrate(<RemixBrowser />, document);
 
 webVitals();
 
-// TODO: Enable once https://github.com/vercel/analytics/issues/1 is resolved
-// if (ENV.ENV === "production") {
-//   inject();
-// } else {
-//   console.log(
-//     "Vercel Analytics is set up, but detected a non-production environment.\n\nPlease note that no analytics events will be sent."
-//   );
-// }
+if (ENV.ENV === "production") {
+  inject();
+} else {
+  console.log(
+    "Vercel Analytics is set up, but detected a non-production environment.\n\nPlease note that no analytics events will be sent."
+  );
+}
