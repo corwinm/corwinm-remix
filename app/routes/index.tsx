c6305d211244531592959c4f181fcb8bd176fbb8
Corwin Marsh <corwinm@users.noreply.github.com>
2023-06-04 23:53:07 -0700

    Default to edge runtime

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,6 +2,8 @@ import AboutSection from "~/components/about-section";
 import ExperienceSection from "~/components/experience-section";
 import Hero from "~/components/hero";
 
+export const config = { runtime: "edge" };
+
 const IndexPage = (): JSX.Element => (
   <>
     <Hero />
