a7f88f60b41d4dd5170475ed438eb8dc555dcb2a
Corwin Marsh <corwinm@users.noreply.github.com>
2023-11-22 22:45:59 -0800

    Add wishlist icons

--- app/routes/wishlist.tsx
+++ app/routes/wishlist.tsx
@@ -1,3 +1,6 @@
+import { faAmazon } from "@fortawesome/free-brands-svg-icons";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import LinkHeader from "~/components/link-header";
 import { ProfileLink } from "~/components/profile-link";
 import ProfileSection from "~/components/profile-section";
@@ -19,35 +22,35 @@ export default function Wishlist() {
         <div className="md:grid md:grid-cols-3 md:gap-4 mt-4">
           <div>
             <h2> Corwin </h2>
-            <ul className="my-2 ml-8 list-disc">
+            <ul className="my-2">
               <li>
                 <ProfileLink href="https://www.amazon.com/hz/wishlist/ls/8LUIV79W3JTK">
-                  Amazon
+                  <FontAwesomeIcon icon={faAmazon} className="w-6" />- Amazon
                 </ProfileLink>
               </li>
               <li>
                 <ProfileLink href="https://www.homedepot.com/list/view/details/shared/ce78b3a0-0459-11ec-8f0b-87c07d1a232d">
-                  Home Depot
+                  <FontAwesomeIcon icon={faHome} className="w-6" />- Home Depot
                 </ProfileLink>
               </li>
             </ul>
           </div>
           <div>
             <h2> Carly </h2>
-            <ul className="my-2 ml-8 list-disc">
+            <ul className="my-2">
               <li>
                 <ProfileLink href="https://www.amazon.com/hz/wishlist/ls/1JB8BVVZ4X45M">
-                  Amazon
+                  <FontAwesomeIcon icon={faAmazon} className="w-6" />- Amazon
                 </ProfileLink>
               </li>
             </ul>
           </div>
           <div>
             <h2> Mayme </h2>
-            <ul className="my-2 ml-8 list-disc">
+            <ul className="my-2">
               <li>
                 <ProfileLink href="https://www.amazon.com/hz/wishlist/ls/23YHQCRCT7HYY">
-                  Amazon
+                  <FontAwesomeIcon icon={faAmazon} className="w-6" />- Amazon
                 </ProfileLink>
               </li>
             </ul>
