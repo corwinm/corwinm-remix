623d6add5f220161902921e3ef09e6a59c2759a6
Corwin Marsh <corwinm@users.noreply.github.com>
2023-11-22 22:25:28 -0800

    Add vercel headers and config for wishlist

--- app/routes/wishlist.tsx
+++ app/routes/wishlist.tsx
@@ -2,6 +2,15 @@ import LinkHeader from "~/components/link-header";
 import { ProfileLink } from "~/components/profile-link";
 import ProfileSection from "~/components/profile-section";
 
+export const config = { runtime: "edge" };
+
+export function headers() {
+  return {
+    "Cache-Control":
+      "s-max-age=2592000, stale-while-revalidate=86400, stale-if-error=604800",
+  };
+}
+
 export default function Wishlist() {
   return (
     <div className="lg:w-[56rem]">
