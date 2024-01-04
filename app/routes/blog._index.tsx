730d9925428eee3807d7fc827ca5447ca7ee0af3
Corwin Marsh <corwinm@users.noreply.github.com>
2024-01-04 00:16:42 -0800

    2023 Year in Review Blog Post

--- app/routes/blog._index.tsx
+++ app/routes/blog._index.tsx
@@ -1,6 +1,7 @@
 import { json } from "@remix-run/node"; // or cloudflare/deno
 import { Link, useLoaderData } from "@remix-run/react";
 import * as introduction from "./blog.introduction.md";
+import * as yearInReview2023 from "./blog.2023-year-in-review.md";
 
 function postFromModule(mod: any) {
   return {
@@ -16,17 +17,17 @@ function postFromModule(mod: any) {
 }
 
 export async function loader() {
-  return json([postFromModule(introduction)]);
+  return json([postFromModule(introduction), postFromModule(yearInReview2023)]);
 }
 
 export default function Index() {
   const posts = useLoaderData<typeof loader>();
 
   return (
-    <ul className="my-12">
+    <ul className="my-12 grid md:grid-cols-2 lg:grid-cols-3 gap-4">
       {posts.map((post) => (
         <li key={post.slug} className="max-w-xs">
-          <Link to={post.slug} className="flex flex-col">
+          <Link to={post.slug} className="flex flex-col h-full">
             {post.img && <img src={post.img} alt="" className="object-cover" />}
             <span className="text-lg my-2">{post.title}</span>
             {post.description ? (
