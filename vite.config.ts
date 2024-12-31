68e68af814d2240260371ba9a82753cc7d52e394
Corwin Marsh <corwinm@users.noreply.github.com>
2024-12-30 19:54:08 -0800

    Feature flag updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
 import mdx from "@mdx-js/rollup";
-import { vitePlugin as remix } from "@remix-run/dev";
-import { installGlobals } from "@remix-run/node";
 import remarkFrontmatter from "remark-frontmatter";
+import { vitePlugin as remix } from "@remix-run/dev";
 import remarkMdxFrontmatter from "remark-mdx-frontmatter";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { vercelPreset } from "@vercel/remix/vite";
 
-installGlobals();
+declare module "@remix-run/node" {
+  // or cloudflare, deno, etc.
+  interface Future {
+    v3_singleFetch: true;
+  }
+}
 
 export default defineConfig({
   server: {
@@ -18,7 +22,17 @@ export default defineConfig({
       remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
     }),
     remix({
+      ignoredRouteFiles: ["**/*.css"],
       presets: [vercelPreset()],
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+        v3_lazyRouteDiscovery: true,
+        v3_singleFetch: true,
+        v3_routeConfig: true,
+        unstable_optimizeDeps: true,
+      },
     }),
     tsconfigPaths(),
   ],
