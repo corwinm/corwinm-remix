9af7fe9cf522c30c18ea33f15d8da7718fe0a2a6
Corwin Marsh <corwinm@users.noreply.github.com>
2025-06-08 14:13:46 -0700

    lint fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
+import mdx from "@mdx-js/rollup";
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-import mdx from "@mdx-js/rollup";
 import remarkFrontmatter from "remark-frontmatter";
 import remarkMdxFrontmatter from "remark-mdx-frontmatter";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(({ command }) => ({
   server: {
